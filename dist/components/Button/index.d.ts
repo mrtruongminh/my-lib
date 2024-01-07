ce770c562bda76ae58a586089685858782e8534a
mrtruongminh <nguyentruongminh0604@gmail.com>
2024-01-08 00:27:29 +0700

    updates

---
+++ dist/components/Button/index.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+import { ButtonProps as MtButtonProps } from "@mantine/core";
+type ButtonProps = {
+    children: React.ReactNode;
+} & MtButtonProps;
+export declare function Button({ children }: ButtonProps): import("react/jsx-runtime").JSX.Element;
+export {};
